3a5f490bf62291a0fdc2d27238ecc7ff19fbdf43
Jonathan Dahan <hi@jonathan.is>
2018-05-21 00:55:37 -0400

    setimmediate version

---
+++ literatepromise.js
@@ -0,0 +1,28 @@
+module.exports = class LiteratePromise {
+  constructor () {
+    this._immediate = null
+    this._queue = []
+  }
+
+  then (onResolve, onReject) {
+    clearImmediate(this._immediate)
+    return this.process().then(onResolve, onReject)
+  }
+
+  process () {
+    return new Promise((resolve, reject) => {
+      const queue = this._queue
+      this._queue = []
+      if (queue.includes('not?')) reject('rejecting: found not?')
+      //console.log(`processing ${queue}`)
+      resolve(`resolved: ${queue}`)
+    })
+  }
+
+  enqueue (data) {
+    clearImmediate(this._immediate)
+    this._queue.push(data)
+    this._immediate = setImmediate(this.process.bind(this))
+    return this
+  }
+}
--- test.js
+++ test.js
@@ -1,69 +1,45 @@
-const Room = require(`./room`)
-const test = require(`ava`)
-const getPort = require(`get-port`)
-const server = require(`./server`)
+// This is a test of the abuse of setImmediate,
+// and understanding if there is a race condition
+// so we can have an l i t e r a t e   p r o m i s e
 
-test.beforeEach(async t => {
-  const port = await getPort()
-  t.context.server = server.create().listen(port)
-  t.context.room = Room.create(`http://localhost:${port}`)
-})
+const LiteratePromise = require(`./literatepromise`)
+const lp = new LiteratePromise()
 
-test.afterEach(t => {
-  t.context.server.close()
-})
+const first = () => {
+  console.log('first enqueue')
+  lp
+    .enqueue('this')
+    .enqueue('is')
+    .enqueue('cool')
+}
 
-test.cb(`once only gets called for existing assertions`, t => {
-  const room = t.context.room
-  const asserts = new Set([ `first`, `second` ])
-  room
-    .assert(`first`)
-    .assert(`second`)
-    .then(() => {
-      room
-        .once(`$number`, ({number}) => {
-          t.true([`first`, `second`].includes(number))
-        })
-      setTimeout(() => {
-        room.assert(`third`).then(() => t.end())
-      }, 150)
-    })
-})
+const second = () => {
+  console.log('second enqueue')
+  lp
+    .enqueue('like')
+    .enqueue('the')
+    .enqueue('coolest')
+    .then(console.log)
+}
 
-test(`await works`, async t => {
-  const room = t.context.room
 
-  const { facts } = await room.assert(`hello`)
-  t.deepEqual(facts, [{assert: `hello`}])
-})
+const third = () => {
+  console.log('third enqueue')
+  lp
+    .enqueue('is')
+    .enqueue('it')
+    .enqueue('not?')
+    .then(null, console.error)
+}
 
-test.cb(`on gets called for all assertions`, t => {
-  const room = t.context.room
-  let asserts = new Set([ `first`, `second`, `third` ])
+const fourth = async () => {
+  console.log('await enqueue')
+  const wat = await lp.enqueue('indeed').enqueue('it').enqueue('is')
+  console.dir(wat)
+}
 
-  room
-    .assert(`first`)
-    .assert(`second`)
-    .then(() => {
-      room
-        .on(`$number`, ({number}) => {
-          t.true(asserts.delete(number))
-        })
-      setTimeout(() => {
-        room.assert(`third`)
-          .then(() => {
-            t.is(asserts.size, 0)
-            t.end()
-          })
-      }, 150)
-    })
-})
+first()
+second()
+third()
+fourth()
 
-test.cb(`an assert with no callback works`, t => {
-  const room = t.context.room
-  room.once(`$what callback assert`, ({what}) => {
-    t.is(what, `no`)
-    t.end()
-  })
-  room.assert(`no callback assert`)
-})
